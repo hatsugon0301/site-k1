83f0438e4ab12605cc940d9c9c6fb33100f1f7c2
hatsugon <>
2021-09-01 20:14:56 +0900

    スクロール調整

--- js/index.js
+++ js/index.js
@@ -128,7 +128,7 @@ $(function() {
             var position = $(this).offset().top;
             var scroll = $(window).scrollTop();
             var windowHeight = $(window).height();
-            if (scroll > position - windowHeight + 500 ) {
+            if (scroll > position - windowHeight + 900 ) {
                 $(this).addClass('fade_in');
             }
         });
