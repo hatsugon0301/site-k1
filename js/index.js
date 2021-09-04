c7288dd288bc0f63bdf27980e5464f2956ee59c9
hatsugon <>
2021-09-04 14:26:59 +0900

    コンセプトループ修正

--- js/index.js
+++ js/index.js
@@ -128,7 +128,7 @@ $(function() {
             var position = $(this).offset().top;
             var scroll = $(window).scrollTop();
             var windowHeight = $(window).height();
-            if (scroll > position - windowHeight + 900 ) {
+            if (scroll > position - windowHeight + 500 ) {
                 $(this).addClass('fade_in');
             }
         });
